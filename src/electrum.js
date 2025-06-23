11dd3ad3b2ee4f626294aad1f5a4838d1397678e
Samigo Silva <Rycchs@gmail.com>
2025-06-22 22:50:13 -0300

    Create electrum.js

---
+++ src/electrum.js
@@ -0,0 +1,26 @@
+const ElectrumClient = require('electrum-client');
+
+class ElectrumInterface {
+  constructor(host = 'electrumx-server.tld', port = 50001, protocol = 'tcp') {
+    this.client = new ElectrumClient(port, host, protocol);
+  }
+
+  async connect() {
+    await this.client.connect();
+    return this.client.server_version('1.4', '1.4.2');
+  }
+
+  async getBalance(address) {
+    const script = bitcoin.address.toOutputScript(address);
+    const hash = bitcoin.crypto.sha256(script);
+    const reversedHash = Buffer.from(hash.reverse());
+    const result = await this.client.blockchainScripthash_getBalance(reversedHash.toString('hex'));
+    return result;
+  }
+
+  async disconnect() {
+    return this.client.close();
+  }
+}
+
+module.exports = ElectrumInterface;
